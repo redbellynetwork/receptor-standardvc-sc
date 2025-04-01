662cef863e85509607762ee6b8ac2252090ebbbf
Shivam kumar sachdeva <98794764+ShivamKumarSa@users.noreply.github.com>
2025-04-01 17:03:14 +0530

    [PROD-3081]: Remove unused function from IBootstrapContractsRegistry interface

--- src/interfaces/IBootstrapContractsRegistry.sol
+++ src/interfaces/IBootstrapContractsRegistry.sol
@@ -2,7 +2,5 @@
 pragma solidity 0.8.22;
 
 interface IBootstrapContractsRegistry {
-    function register(string calldata name, address contractAddress) external returns (bool);
-
     function getContractAddress(string memory contractName) external view returns (address);
 }
